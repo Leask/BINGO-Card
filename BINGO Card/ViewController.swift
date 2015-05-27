b5832e5c3788b50623b76f00dc126f02ad64e4c2
Leask Huang <i@leaskh.com>
2015-05-28 01:20:24 +0800

    Initial Commit

---
+++ BINGO Card/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BINGO Card
+//
+//  Created by Leask Wong on 5/28/15.
+//  Copyright (c) 2015 Leask Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BINGO CardTests/BINGO_CardTests.swift
@@ -0,0 +1,36 @@
+//
+//  BINGO_CardTests.swift
+//  BINGO CardTests
+//
+//  Created by Leask Wong on 5/28/15.
+//  Copyright (c) 2015 Leask Wong. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class BINGO_CardTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
